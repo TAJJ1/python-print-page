e4fad2ceed9df13f1a5aa5cba62a18d42049fcf8
TAJJ1 <stajj336@gmail.com>
2021-02-08 02:59:11 -0800

    hi this is my first python class task

---
+++ Variables/variable.py
@@ -0,0 +1,56 @@
+Cname='my company name is'
+print(Cname)
+name='Emodian Tech'
+print(name)
+Mcompany='my company in pakistan'
+print(Mcompany)
+etech=1
+print(etech)
+colorboard='the color of board is'
+print(colorboard)
+whiteboard='brown'
+print(whiteboard)
+LN='the leatest news is'
+print(LN)
+under_score='ARY news'
+print(under_score)
+num1=78768
+num2=7456
+drth=num1-num2
+print(drth)
+f=3
+r=f -3
+print(r)
+add1=7784+345345/345345+345345+345345
+add2=98798357+345345+345345+34535
+addition=add1+add2
+print(addition)                                     
+
+print('                                                                                                                                                                   ////////////////////////////////////////////////////////////                                                                                                        ')
+print('                                                                                                                                                                  //                                                         //                                                                                                        ')
+print('                                                                                                                                                                 //                                                         //                                                                                                         ')
+print('                                                                                                                                                                //                   HELLO GUYS                            //                                                                                                         ')
+print('                                                                                                                                                               //                                                         //                                                                                                         ')
+print('                                                                                                                                                              //            MY NAME IS SYED ARMAN ALI SHAH               //                                                                                                         ')
+print('                                                                                                                                                             //                      IAM                                //                                                                                                         ')
+print('                                                                                                                                                            //              USING PYHTON FOR MY PROGRAMMING            //                                                                                                         ')
+print('                                                                                                                                                           //                  IAM BIGGNER IN PYTHON                  //                                                                                                         ')
+print('                                                                                                                                                          //                  DATE 26/11/2018                        //                                                                                                         ')
+print('                                                                                                                                                         //                                                         //                                                                                                         ')
+print('                                                                                                                                                        //                                                         //                                                                                                        ')
+print('                                                                                                                                                       //                                                         //                                                                                                        ')
+print('                                                                                                                                                      //                                                         //                                                                                                        ')
+print('                                                                                                                                                     //                                                         //                                                                                                        ')
+print('                                                                                                                                                    /|_________________________________________________________||                                                                                                        ')
+print('                                                                                                                                                   /|__________________________________________________________||                                                                                                        ')
+print('                                                                                                                                                 //____________________________________________________________//                                                                                                        ')
+print('                                                                                                                                               //                                                            _//|                                                                                                        ')
+print('                                                                                                                                             // __  ___  ___  ___  ___   ___   ______  ______  ___ ____ __ _//  |                                                                                                        ')
+print('                                                                                                                                          //     ___  ___   ___  ____  -___ ____  _____   _____  ____ _ _//    /                                                                                                        ')
+print('                                                                                                                                        //     ___  ____  ____  ____  ____  ___   -___  ____  ___ ___ _//    /                                                                                                        ')
+print('                                                                                                                                      //    ___  ____   ____  _____  _____  _____  _____  _____  -_ _//    /                                                                                                         ')
+print('                                                                                                                                    //                                                            _//    /                                                                                                        ')
+print('                                                                                                                                  //_____________________________________________________________//    /                                                                                                        ')
+print('                                                                                                                                  //________0_0_0________________________________________________//  /                                                                                                        ')
+print('                                                                                                                                  //_____________________________________________________________/ /                                                                                                        ')                                            
+
---
+++ day2.py
@@ -0,0 +1,18 @@
+name='My Name is Syed Arman Ali Shah'
+print(name)
+fname='my father name is syed farman ali shah'
+print(fname)
+cinc=' my cnic:7140166664129'
+print(cinc)
+blong='i belong to yasin'
+print(blong)
+title_good='this is my phone n0 031760970719'
+print(title_good)
+name="heloo word"
+print(type(name))
+sec=4
+print(type(sec))
+thid=30.8
+print(type(thid))
+fov=49j
+
